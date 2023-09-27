df6e808922e7fb63e9588d48cc92128748cdae83
Dilena Koplande <koplande@yahoo.com>
2023-09-27 10:43:53 -0500

    jkjhbgvf

--- File managment/File managment.cpp
+++ File managment/File managment.cpp
@@ -7,6 +7,14 @@
 int main()
 {
 	ManageProperties manage("Properties.txt");
-	for (Properties prop : manage.get())
-		std::cout << prop;
- }
+	ManageOwners manageO("Owners.txt");
+	int i = 0;
+	for (Owners owner : manageO.get()) {
+		std::vector<Properties> arr = manage.getPropertiesByOwner(i);
+		std::cout << owner.user.first << " ";
+		for (Properties prop : arr)
+			std::cout << prop.address<<" ";
+		puts("");
+		i++;
+	}
+}
